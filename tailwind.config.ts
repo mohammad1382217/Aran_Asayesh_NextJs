7339600e7ec4884dd778a3e3a06d3f98b900ef42
Mohammad Mohammadzade <mohammad1382217@gmail.com>
2024-04-09 14:39:02 +0330

    add aspect-ratio component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,8 +23,18 @@ const config = {
           "0%,70%,100%": { opacity: "1" },
           "20%,50%": { opacity: "0" },
         },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
       animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
         "caret-blink": "caret-blink 1.25s ease-out infinite",
       },
     },
